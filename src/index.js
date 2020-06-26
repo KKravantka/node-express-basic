a8862e2cefd49824a054ec3e33357cc452b87b17
KKravantka <kevin.kravantka@coastalcloud.us>
2020-06-26 13:50:30 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('', (req, res) =>{
+    console.log(req) // request to endpoint
+
+    res.send('sent from node :)')
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
